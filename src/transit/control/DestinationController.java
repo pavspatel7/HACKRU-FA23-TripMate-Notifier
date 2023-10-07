03b46f4f0482d36b84508890dfe26d370ff8e872
Pavitra Patel <Pavs.network7@gmail.com>
2023-10-07 17:18:58 -0400

    Destination Setup

---
+++ src/transit/control/DestinationController.java
@@ -0,0 +1,15 @@
+package transit.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class DestinationController {
+
+    @FXML
+    private ListView<?> destinationList;
+
+    @FXML
+    private TextField destinationName;
+
+}
