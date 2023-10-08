d50b65f80481aa82a2a4a9d7a2df215f10c7b16c
Pavitra Patel <Pavs.network7@gmail.com>
2023-10-07 20:04:09 -0400

    UI added

---
+++ src/transit/control/TrainController.java
@@ -0,0 +1,18 @@
+package transit.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
+public class TrainController {
+
+    @FXML
+    private ListView<?> trainList;
+
+    @FXML
+    private Text transfer;
+
+    @FXML
+    private Text travelTime;
+
+}
